b1ea377121ea9cc2ddcc03083488a77f97ceac86
Xavier <xavierpe@qq.com>
2020-07-09 15:27:21 +0800

    初始化项目

---
+++ eureka2/src/main/java/org/xavier/Eureka2Application.java
@@ -0,0 +1,22 @@
+package org.xavier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * 描述信息：<br/>
+ *
+ * @author Xavier
+ * @version 1.0
+ * @date 2020/07/08
+ * @since Jdk 1.8
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka2Application {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(Eureka2Application.class, args);
+    }
+}
